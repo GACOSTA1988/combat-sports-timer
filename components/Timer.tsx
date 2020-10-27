a547c18704694511b6671538f0ec0d2ae8fd3126
GACOSTA1988 <georgeacostar@gmail.com>
2020-10-26 21:53:50 -0700

    add interface and hook to Timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+interface State {
+    time: number
+    seconds: number
+    minutes: number
+}
+
 const Timer = () => {
+    const [state, setState] = React.useState<State>({
+        time: 60,
+        seconds: 0,
+        minutes: 1,
+    })
     return <View>
-        <Text>Hellooooooooooooo</Text>
+        <Text>{`${state.minutes}: ${state.seconds <= 10 ? `0${state.seconds}` : state.seconds}`}</Text>
     </View>
 }
 
