cdb5e0df0cf97b858b44443c0f795368e6701380
GACOSTA1988 <georgeacostar@gmail.com>
2020-10-26 21:45:48 -0700

    delete files and create base component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text, View } from './components/Themed';
-
+import { Text, View } from 'react-native';
+import Timer from './components/Timer'
 // import Navigation from './navigation';
 
 export default function App() {
@@ -8,8 +8,6 @@ export default function App() {
 
 
   return (
-    <View>
-      <Text>Hellooooooooooooo</Text>
-    </View>
+    <View style={{ flex: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}><Timer /></View>
   );
 }
---
+++ components/Timer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Timer = () => {
+    return <View>
+        <Text>Hellooooooooooooo</Text>
+    </View>
+}
+
+export default Timer
