441628231e7be47f5853ee539a2c6bac4dd6d8c8
GACOSTA1988 <georgeacostar@gmail.com>
2020-10-31 16:52:49 -0700

    add state and use effect to timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-interface State {
+interface TimerState {
     time: number
     seconds: number
     minutes: number
 }
 
-const Timer = () => {
-    const [state, setState] = React.useState<State>({
-        time: 60,
-        seconds: 0,
-        minutes: 1,
+type TimerProps = {
+    startTime: "number"
+}
+
+const Timer: React.FC<Props> = ({ time }) => {
+    const [state, setState] = React.useState<TimerState>({
+        time,
+        seconds: time - Math.floor((time - 1) / 60) * 60 - 1,
+        minutes: Math.floor((time - 1) / 60),
     })
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            if (state.time === 0) {
+                return
+            }
+
+            setState({
+                time: state.time - 1,
+                minutes: Math.floor((state.time - 1) / 60),
+                seconds: state.time - Math.floor((state.time - 1) / 60) * 60 - 1,
+            })
+        }, 1000)
+    }, [state.time])
+
     return <View>
-        <Text>{`${state.minutes}: ${state.seconds <= 10 ? `0${state.seconds}` : state.seconds}`}</Text>
+        <Text>{`${state.minutes}:${state.seconds <= 10 ? `0${state.seconds}` : state.seconds}`}</Text>
     </View>
 }
 
