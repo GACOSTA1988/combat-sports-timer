266e53ccfe7b4d9ca99a34ed3e6795196f6293f2
GACOSTA1988 <georgeacostar@gmail.com>
2020-11-01 17:55:59 -0800

    style container

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Timer from './components/Timer'
-// import Navigation from './navigation';
 
 export default function App() {
-
   const oneRound = 300
 
   return (
-    <View style={{ flex: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}><Timer time={oneRound} /></View>
+    <View style={styles.container}><Timer time={oneRound} /></View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+    padding: 20,
+    marginVertical: 25,
+  },
+});
